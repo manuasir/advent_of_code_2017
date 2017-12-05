c76b51a7b92720261980aa27dc96718ecef58b47
manuasir <manu.asi2@gmail.com>
2017-12-05 19:53:51 +0100

    completing day 5 :white_check_mark:

--- day_5/index.js
+++ day_5/index.js
@@ -17,3 +17,24 @@ exports.instructionSteps = (input) => {
   }while(nextIndex<instructions.length)
   return count
 }
+
+exports.instructionStepsV2 = (input) => {
+  const instructions = input.split('\n')
+  for(let i=0;i<instructions.length;i++) {
+    instructions[i] = Number(instructions[i])
+  }
+  let count = 0
+  let index = 0
+  let nextIndex = 0
+  do{
+
+    nextIndex = index + instructions[index]
+    if(instructions[index]>=3)
+      instructions[index]--
+    else
+      instructions[index]++
+    index=nextIndex
+    count++
+  }while(nextIndex<instructions.length)
+  return count
+}
--- test/testDay5.js
+++ test/testDay5.js
@@ -12,7 +12,13 @@ describe('Testing day 5', () => {
     assert(require('../day_5/index'))
     require('../day_5/index').instructionSteps(require('../day_5/input').liteInput).should.be.equal(5)
     require('../day_5/index').instructionSteps(require('../day_5/input').input).should.be.equal(339351)
-
+    done()
+  })
+  it('second challenge', (done) => {
+    assert(should)
+    assert(require('../day_5/index'))
+    require('../day_5/index').instructionStepsV2(require('../day_5/input').liteInput).should.be.equal(10)
+    require('../day_5/index').instructionStepsV2(require('../day_5/input').input).should.be.equal(24315397)
     done()
   })
 
