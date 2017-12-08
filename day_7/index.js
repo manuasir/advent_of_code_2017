79d8bb1dea5de50672a937253c2ae1ff2f80991a
manuasir <manu.asi2@gmail.com>
2017-12-08 01:52:35 +0100

    completing day 7 :white_check_mark:

--- day_7/index.js
+++ day_7/index.js
@@ -2,13 +2,15 @@ exports.makeTree = (input) => {
   let lines = input.trim().split('\n')
   let tree = []
   for (let line of lines) {
+    const currentValue = line.split('->')[0].split(' ')[0].trim()
     if (line.indexOf('->') !== -1) {
-      const currentValue = line.split('->')[0].split(' ')[0]
-      const currentChildren = line.split('->')[1].split(',')
-      console.log(currentValue+" : "+currentChildren)
-
-
+      const currentChildren = line.split('->')[1].split(',').map(o=>o.trim())
+      tree.push({value:currentValue, children:currentChildren})
+    } else {
+      tree.push({value:currentValue})
     }
   }
-  return tree[0].value
+  const arr1 = tree.map((o)=> { return o.value})
+  const arr2 = tree.map((o) => { if (o.children) return o.children}).join().split(',')
+  return arr1.filter(x => arr2.indexOf(x) === -1).join()
 }
--- test/testDay7.js
+++ test/testDay7.js
@@ -12,9 +12,7 @@ describe('Testing day 7', () => {
     assert(require('../day_7/index'))
     require('../day_7/input').input.trim().split('\n').length.should.be.equal(1078)
     require('../day_7/index').makeTree(require('../day_7/input').liteInput).should.be.equal('tknk')
-    // require('../day_7/index').getMax(require('../day_6/input').input.split('\t')).value.should.be.equal(15)
-    // require('../day_7/index').memoryDistribution(require('../day_6/input').liteInput)[0].should.be.equal(5)
-    // require('../day_7/index').memoryDistribution(require('../day_6/input').input)[0].should.be.equal(12841)
+    require('../day_7/index').makeTree(require('../day_7/input').input).should.be.equal('aobgmc,rqwgj')
     done()
   })
   it('second challenge', (done) => {
